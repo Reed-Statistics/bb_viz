cbcbf6936d03014dd9da7ee5fc77a2188dc3acf4
rileyleonard99 <leonardr@reed.edu>
2020-04-26 18:58:42 -0700

    Sample spray chart construction

--- arenado-example.R
+++ arenado-example.R
@@ -2,7 +2,6 @@
 
 library(tidyverse)
 library(baseballr)
-library(ggthemes)
 
 
 ## Player ID
@@ -18,17 +17,12 @@ arenado <- scrape_statcast_savant(
   playerid = 571448,
   player_type = "batter")
 
-## Mutates
-
-arenado <- arenado %>%
-  mutate (spray_angle = round((atan((hc_x-125.42)/(198.27-hc_y))*180/pi*.75), 1))
-
 
 ## Wrangle
 
 arenado <- arenado %>%
   filter(description %in% c("hit_into_play", "hit_into_play_no_out", "hit_into_play_score")) %>%
-  mutate(hit_type = ifelse(events == "single", "Single",
+  mutate(hit_result = ifelse(events == "single", "Single",
                                     ifelse(events == "double", "Double",
                                            ifelse(events == "triple", "Triple",
                                                   ifelse(events == "home_run", "Home Run",
@@ -40,31 +34,51 @@ arenado <- arenado %>%
 
 arenado %>%
   ggplot(aes(x = hc_x, y = -hc_y)) +
-  geom_segment(x = 128, xend = 20, y = -208, yend = -100, size = 0.7, color = "grey50") +
-  geom_segment(x = 128, xend = 236, y = -208, yend = -100, size = 0.7, color = "grey50") +
+  geom_segment(x = 128, xend = 20, y = -208, yend = -100, size = 0.7, color = "grey66", lineend = "round") +
+  geom_segment(x = 128, xend = 236, y = -208, yend = -100, size = 0.7, color = "grey66", lineend = "round") +
   geom_curve(x = 77, xend = 178, y = -155, yend = -156,
-             curvature = -0.65, linetype = "dotted", color = "grey 50") +
+             curvature = -0.65, linetype = "dotted", color = "grey66") +
   coord_fixed() +
-  geom_point(aes(color = hit_type), alpha = 0.75, size = 2) +
+  geom_point(aes(color = hit_result), alpha = 0.75, size = 2) +
   scale_x_continuous(limits = c(25, 225)) +
   scale_y_continuous(limits = c(-225, -25)) +
   labs(title = "Nolan Arenado Spray Chart",
        subtitle = "2019 MLB Season",
-       color = "Hit Type") +
+       color = "Hit Result") +
+  theme_void() +
+  theme(plot.background = element_rect(fill = "grey96"),
+        plot.title = element_text(hjust = 0.5), 
+        plot.subtitle = element_text(hjust = 0.5, face = "italic"))
+
+  
+## Sample Plot (w / Density)
+
+arenado %>%
+  ggplot(aes(x = hc_x, y = -hc_y)) +
+  geom_segment(x = 128, xend = 18, y = -208, yend = -100, size = 0.7, color = "grey50", lineend = "round") +
+  geom_segment(x = 128, xend = 238, y = -208, yend = -100, size = 0.7, color = "grey50", lineend = "round") +
+  geom_curve(x = 76, xend = 179, y = -155, yend = -156,
+             curvature = -0.65, linetype = "dotted", color = "grey 50") +
+  coord_fixed() +
+  geom_point(aes(color = hit_result), alpha = 0.75, size = 2) +
+  geom_density_2d(size = 1, alpha = 0.5) +
+  scale_x_continuous(limits = c(25, 225)) +
+  scale_y_continuous(limits = c(-225, -25)) +
+  labs(title = "Nolan Arenado Spray Chart",
+       subtitle = "2019 MLB Season",
+       color = "Hit Result") +
   theme_void() + theme(plot.background = element_rect(fill = "grey95")) +
   theme(plot.title = element_text(hjust = 0.5), 
         plot.subtitle = element_text(hjust = 0.5, face = "italic"))
 
-  
-## Sample Plot (Density)
-
-
 
 ## Shiny App Spray Chart Idea:
-  
+
+# color by: appropriate factor colors (i.e. out = red)
 # input: player name
 # input: date range
-# color by options: hit result, launch angle, exit velocity
+# color by options: hit result, launch angle, exit velocity, estimated BA
 # filter by options: hit result
 # ouput: plot, title, summary stats
-# option: density plot
+# toggle option: density overlay
+# warning: comtains missing values
