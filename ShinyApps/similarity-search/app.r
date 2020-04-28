23732d41e40cb5b763fdf87dba353685eeb6bcad
Grayson White <graysonwhite13@gmail.com>
2020-04-28 15:18:20 -0700

    start app.r

--- ShinyApps/similarity-search/app.r
+++ ShinyApps/similarity-search/app.r
@@ -1,7 +1,21 @@
+# Load all necessary packages and functions here
 library(shiny)
+library(shinythemes)
+library(tidyverse)
+library(baseballr)
+library(plotly)
+library(viridis)
+library(scales)
+library(pitchRx)
+
+`%notin%` <- Negate(`%in%`)
+
+# Load all static dataframes here
+players <- players
 
 # User interface
-ui <- fluidPage()
+ui <- fluidPage(theme = shinytheme("flatly"),
+                titlePanel("An Interactive Baseball Visualization Application"))
 
 # Server function
 server <- function(input, output){}
