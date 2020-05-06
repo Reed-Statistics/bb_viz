4aeec1846ddae5b7f6dae4e5d0c3cae83fe8a0cd
rileyleonard99 <leonardr@reed.edu>
2020-05-06 13:18:01 -0700

    Fixing players lists (again)

--- Adding Positions.R
+++ Adding Positions.R
@@ -19,28 +19,26 @@ positions <- Fielding %>%
             B = sum(POS != "P"))
 
 positions <- left_join(positions, names, by = "playerID") %>%
-  select(P, B, full_name.x) %>%
-  rename(full_name = full_name.x)
+  select(playerID, P, B, full_name)
 
 positions <- positions %>%
   mutate(player_type = ifelse(P > B, "Pitcher",
                        ifelse(B > P, "Batter", "Both")))
 
 positions <- positions %>%
-  select(full_name, player_type)
-
-write_csv(positions, "/home/leonardr/baseball_viz/positions.csv")
+  select(playerID, player_type) %>%
+  rename(key_bbref = playerID)
   
-positions <- read_csv("/home/leonardr/baseball_viz/positions.csv")
 
 
 # Improved Player List
 
 players_list <- get_chadwick_lu() %>%
-  select(key_mlbam, name_last, name_first, mlb_played_last)
+  select(key_mlbam, name_last, name_first, mlb_played_last, key_bbref)
 
 players_list <- players_list %>%
   drop_na(key_mlbam) %>%
+  drop_na(key_bbref) %>%
   drop_na(mlb_played_last)
 
 players_list <- players_list %>%
@@ -54,42 +52,20 @@ players_list <- players_list %>%
 
 write_csv(players_list, "/home/leonardr/baseball_viz/players.csv")
 
-players_positions <- left_join(players_list, positions, by = "full_name")
+players_positions <- left_join(players_list, positions, by = "key_bbref")
 
 players_positions <- players_positions %>%
   distinct(id, .keep_all = TRUE)
 
 
-# Fixing NAs and Duplicates
+# Fixing NAs
 
 players_positions <- players_positions %>%
-  drop_na(player_type) %>%
-  filter(full_name != "Carlos Martinez",
-         full_name != "Joe Kelly",
-         full_name !=  "Luis Garcia",
-         full_name != "Adam Eaton",
-         full_name != "Carlos Perez",
-         full_name != "Charlie Morton",
-         full_name != "Chris Hatcher",
-         full_name != "Chris Smith",
-         full_name != "Chris Young",
-         full_name != "Erik Johnson",
-         full_name != "Joe Smith",
-         full_name != "Jose Bautista",
-         full_name != "Jose Fernandez",
-         full_name != "Jose Ramirez",
-         full_name != "Josh Fields",
-         full_name != "Kyle Waldrop",
-         full_name != "Nelson Cruz",
-         full_name != "Ryan Braun")
+  drop_na(player_type)
 
-more_players_positions <- read_csv("Player Types - Sheet1.csv")
+more_players_positions <- read_csv("Player Types - Sheet1 (1).csv")
 
 players_positions <- rbind(players_positions, more_players_positions)
-
-players_positions <- players_positions %>%
-  filter(player_type != "Both",
-         player_type != "Catcher")
 
 pitchers <- players_positions %>%
   filter(player_type == "Pitcher")
@@ -125,11 +101,6 @@ batters <- players_positions %>%
 
 batters <- left_join(batters, players_list2, by = "id")
 
-batters <- batters %>%
-  select(id, full_name.x, mlb_played_last.x, player_type, key_bbref) %>%
-  rename(full_name = full_name.x,
-         mlb_played_last = mlb_played_last.x)
-
 batting <- Batting %>%
   filter(yearID > 2014) %>%
   rename(key_bbref = playerID) %>%
@@ -145,6 +116,21 @@ batters <- batters %>%
   select(id, full_name, mlb_played_last, player_type, balls_in_play) %>%
   filter(balls_in_play > 0)
 
+
+# Fixing NAs
+
+batters <- batters %>%
+  drop_na(balls_in_play)
+
+more_batters <- read_csv("Player Types - Sheet2.csv")
+
+batters <- rbind(batters, more_batters)
+
+
+# Filtering Batters
+
+batters <- batters %>%
+  filter(balls_in_play > 0)
   
 
 # Write CSV
